e07bf87608c21be7d612f2a5b1aac3c180008d6c
Manjari Dhoundiyal <manjaridhoundiyal@shubham.local>
2023-05-01 22:45:04 +0530

    updated test cases

--- src/App.js
+++ src/App.js
@@ -1,62 +1,17 @@
 import { useEffect, useState } from 'react';
 
-const data = [
-  {
-    id: '1',
-    name: 'Pilsner',
-    minimumTemperature: 4,
-    maximumTemperature: 6,
-  },
-  {
-    id: '2',
-    name: 'IPA',
-    minimumTemperature: 5,
-    maximumTemperature: 6,
-  },
-  {
-    id: '3',
-    name: 'Lager',
-    minimumTemperature: 4,
-    maximumTemperature: 7,
-  },
-  {
-    id: '4',
-    name: 'Stout',
-    minimumTemperature: 6,
-    maximumTemperature: 8,
-  },
-  {
-    id: '5',
-    name: 'Wheat beer',
-    minimumTemperature: 3,
-    maximumTemperature: 5,
-  },
-  {
-    id: '6',
-    name: 'Pale Ale',
-    minimumTemperature: 4,
-    maximumTemperature: 6,
-  },
-];
-
 function App() {
   const [items, setItems] = useState({});
 
   useEffect(() => {
-    const request = () =>
-      data.forEach((product) => {
-        fetch(`http://localhost:8081/temperature/${product.id}`)
-          .then((response) => response.json())
-          .then((response) =>
-            setItems((prevItems) => ({
-              ...prevItems,
-              [product.id]: {
-                ...product,
-                ...response,
-              },
-            }))
-          );
-      });
+
+    const request = () =>{
+      fetch(`http://localhost:8081/temperature`)
+      .then((response) => response.json())
+      .then((response) =>
+      setItems(response)
+      );
+    }
 
     setInterval(request, 5000);
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,92 @@
-import {render} from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react'
 import App from './App';
+
+const beerMockData =[
+    {
+      id: '1',
+      name: 'Pilsner',
+      minimumTemperature: 4,
+      maximumTemperature: 6,
+      temperature: 5
+    },
+    {
+      id: '2',
+      name: 'IPA',
+      minimumTemperature: 5,
+      maximumTemperature: 6,
+      temperature: 3,
+    },
+    {
+      id: '3',
+      name: 'Lager',
+      minimumTemperature: 4,
+      maximumTemperature: 7,
+      temperature: 8
+    }
+  ];
 
 describe('<App />', () => {
     it('renders without errors', () => {
         render(<App />)
+    });
+
+    it('renders the heading Beers', () => {
+        render(<App />);
+        const heading = screen.getByRole('heading', { level: 2 }, { name: 'Beers' });
+        expect(heading).toBeInTheDocument();
+    });
+    
+    it('fetches and displays the data from the API', async () => {
+        jest.spyOn(global, 'fetch').mockImplementation(() =>
+          Promise.resolve({
+            json: () => Promise.resolve(beerMockData),
+          })
+        );
+    
+        render(<App />);
+        await waitFor(() => expect(global.fetch).toHaveBeenCalledTimes(1));
+    
+        const rows = screen.getAllByRole('row');
+        expect(rows).toHaveLength(4);
+    
+        const headers = screen.getAllByRole('columnheader');
+        expect(headers[0]).toHaveTextContent('Product');
+        expect(headers[1]).toHaveTextContent('Temperature');
+        expect(headers[2]).toHaveTextContent('Status');
+    
+        const cells = screen.getAllByRole('cell');
+        expect(cells[0]).toHaveTextContent('Pilsner');
+        expect(cells[1]).toHaveTextContent('5');
+        expect(cells[2]).toHaveTextContent('all good');
+        expect(cells[3]).toHaveTextContent('IPA');
+        expect(cells[4]).toHaveTextContent('3');
+        expect(cells[5]).toHaveTextContent('too low');
+        expect(cells[6]).toHaveTextContent('Lager');
+        expect(cells[7]).toHaveTextContent('8');
+        expect(cells[8]).toHaveTextContent('too high');
+    
+        global.fetch.mockRestore();
+    });
+
+
+    it('fetches the data from the API every 5 seconds', async () => {
+        jest.useFakeTimers();
+
+        jest.spyOn(global, 'fetch').mockImplementation(() =>
+        Promise.resolve({
+            json: () => Promise.resolve(beerMockData),
+        })
+        );
+
+        render(<App />);
+        await waitFor(() => expect(global.fetch).toHaveBeenCalledTimes(1));
+
+        jest.advanceTimersByTime(5000);
+        await waitFor(() => expect(global.fetch).toHaveBeenCalledTimes(2));
+
+        jest.advanceTimersByTime(5000);
+        await waitFor(() => expect(global.fetch).toHaveBeenCalledTimes(3));
+
+        global.fetch.mockRestore();
     });
 });
